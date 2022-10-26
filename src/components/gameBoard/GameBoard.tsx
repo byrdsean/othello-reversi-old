563db719d0eb584aeaa2a12831a748d1925f9061
Sean Byrd <byrd.sean1015@gmail.com>
2022-10-25 20:01:33 -0400

    Adjusted code to store metadata for individual tiles. Added functionality to highlight a tile that is a valid placement option for the player.

--- src/components/gameBoard/GameBoard.tsx
+++ src/components/gameBoard/GameBoard.tsx
@@ -5,18 +5,19 @@ import InitialGameBoard from "../../api/initialGameBoard.json";
 import { PlayerEnum } from "../../models/playerEnum";
 import { expandBoard, flattenBoard } from "./util/BoardUtilities";
 import { validateTile } from "./tile/util/Validation";
+import { TileMetaData } from "./models/TileMetaData";
 
 const GameBoard = () => {
-  const [boardData, setBoardData] = useState<number[]>([]);
+  const [boardData, setBoardData] = useState<TileMetaData[]>([]);
   const [currentPlayer, setCurrentPlayer] = useState<number>(0);
 
   const setTile = (x: number, y: number) => {
     //Expand the board into a 2D array
-    let fullBoard: number[][] = expandBoard(boardData);
+    let fullBoard: TileMetaData[][] = expandBoard(boardData);
 
     //Only set the tile if it is currently invalid
-    if (!validateTile(fullBoard[y][x])) {
-      fullBoard[y][x] = currentPlayer;
+    if (!validateTile(fullBoard[y][x].value)) {
+      fullBoard[y][x].value = currentPlayer;
       setBoardData(flattenBoard(fullBoard));
     }
   };
@@ -24,8 +25,18 @@ const GameBoard = () => {
   useEffect(() => {
     setCurrentPlayer(PlayerEnum.PLAYER_BLACK);
 
-    //Load the game board
-    setBoardData(flattenBoard(InitialGameBoard.initialBoard));
+    //Initialize the 2d game board to store metadata for each tile
+    let initialBoard = InitialGameBoard.initialBoard.map<TileMetaData[]>(
+      (row) => {
+        return row.map<TileMetaData>((col) => {
+          return {
+            value: col,
+            isValidPlacement: Math.floor(Math.random() * 10) / 2 === 0,
+          };
+        });
+      }
+    );
+    setBoardData(flattenBoard(initialBoard));
   }, []);
 
   return (
@@ -34,10 +45,10 @@ const GameBoard = () => {
       <div id="gameBoard">
         {expandBoard(boardData).map((row, rIndex) => (
           <div key={`r${rIndex}`} className="row">
-            {row.map((tileValue, cIndex) => (
+            {row.map((tileData, cIndex) => (
               <Tile
                 key={`r${rIndex}c${cIndex}`}
-                value={tileValue}
+                data={tileData}
                 xCoord={cIndex}
                 yCoord={rIndex}
                 setTile={setTile}
